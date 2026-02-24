3eb82f3fb63179cda0da305d0e253b37befde6a0
Annika <annikas@andrew.cmu.edu>
2026-02-24 10:19:02 -0500

    multiplied

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -22,6 +22,16 @@ export default function QueryProcessor(query: string): string {
       
   }
 
+    if (query.toLowerCase().includes("multiplied")) {
+      const numbers = query.match(/\d+/g)?.map(Number) || [];
+      let multiply = 1;
+      for (let i = 0; i < numbers.length; i++) {
+              multiply = multiply * numbers[i];
+      } 
+      return multiply.toString(10);  
+      
+  }
+
   if (query.toLowerCase().includes("plus")) {
       const numbers = query.match(/\d+/g)?.map(Number) || [];
       let sum = 0;
